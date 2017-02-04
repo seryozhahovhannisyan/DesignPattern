f9f5cda5a2d0334bc3eef184ded40b3f1beb2a28
seryozha.hovhnnisyan@gmail.com <hovhannisyan.arthur@hotmail.com>
2017-02-04 20:27:56 +0400

    Mediator/Memento/Observer

--- src/main/java/seryozha/hovhannisyan/DesignPatternApplication.java
+++ src/main/java/seryozha/hovhannisyan/DesignPatternApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesignPatternApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//v
 		//SpringApplication.run(DesignPatternApplication.class, args);
 	}
 }
